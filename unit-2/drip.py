678ad12e6cdccdb7acd7ec29fda70accce180a43
fall3n <yasharya482190@gmail.com>
2025-02-22 09:02:23 +0530

    Completed question 4 unit-2

--- unit-2/drip.py
+++ unit-2/drip.py
@@ -8,45 +8,72 @@ def main():
     asymm = 22.4
     colTerm = 0.751
 
-    stable = []
-    Zval = []
-    Nval = []
-    proton = []
+    ZBeta = []  # Z for beta stablility line
+    NBeta = []
+
+    Zproton = []
+    NProton = []
+
+    NNeutron = []
+    ZNeutron = []
 
     for A in range(1, 251):
 
-        if A % 2 == 0:
-            Z = A / 2
-
-        else:
-            Z = (A - 1) / 2
-
-        Z = int(Z)
-
-        N = A - Z
-
         beta = betastable(A, colTerm, asymm)
-        pro = protondrip(A, Z, colTerm, asymm, volTerm, surfaceTerm)
-        proton.append(pro)
-        stable.append(beta)
-        Zval.append(Z)
-        Nval.append(N)
+        pro = protondrip(A, colTerm, asymm, volTerm, surfaceTerm)  # Value of Z
+        neu = neutrondrip(A, volTerm, surfaceTerm, asymm, colTerm)  # Value of N
 
-    plt.plot(Nval, stable)
-    plt.plot(Nval, proton)
-    plt.legend(["Beta stablility line ", "Proton stablility line "])
-    plt.xlabel("N no of neutron")
-    plt.ylabel("Z no of proton")
+        # Beta stablility line
+        Z = (A - beta) / 2
+        N = A - Z
+        # print(f"The value of N is {N} and the value of Z is {Z}")
+        ZBeta.append(Z)
+        NBeta.append(N)
+
+        # Proton Drip line
+        N = A - pro
+        NProton.append(N)
+        Zproton.append(pro)
+        # print(f"The value of N is {N} and the value of Z is {pro}")
+
+        # Neutron Drip line
+        Z = A - neu
+        NNeutron.append(neu)
+        ZNeutron.append(Z)
+        # print(f"The value of N is {Z} and the value of Z is {neu}")
+
+
+    plt.plot(NBeta,ZBeta)
+    plt.plot(NProton,Zproton)
+    plt.plot(NNeutron,ZNeutron)
+    plt.legend(["Beta stablility line ", "Proton drip line ", "Neutron drip line "])
+    plt.xlabel("Z no of neutron")
+    plt.ylabel("N no of proton")
     plt.show()
 
-def coefbeta(A, colterm, asymm) -> float:
-    coef = 2 * colterm * A ** (2 / 3) / (colterm * A ** (2 / 3) + 12 * asymm)
+
+def coefalpha(A, volTerm, surfaceTerm, colTerm, asymm) -> float:
+
+    alpha = (
+        volTerm
+        + 3 * asymm
+        - (2 / 3) * surfaceTerm / (A ** (1 / 3))
+        + colTerm * A ** (2 / 3) / 3
+    )
+
+    alpha = alpha / ((colTerm / 3 * A ** (2 / 3)) + 4 * asymm)
+
+    return alpha
+
+
+def coefbeta(A, colTerm, asymm) -> float:
+    coef = 2 * colTerm * A ** (2 / 3) / (colTerm * A ** (2 / 3) + 12 * asymm)
     return coef
 
 
-def coefgamma(A, volTerm, surfaceTerm, asymm, colterm) -> float:
+def coefgamma(A, volTerm, surfaceTerm, asymm, colTerm) -> float:
     coef = (volTerm + 3 * asymm - (2 / 3) * surfaceTerm * A ** (-1 / 3)) / (
-        (1 / 3) * colterm * A ** (2 / 3) + 4 * asymm
+        (1 / 3) * colTerm * A ** (2 / 3) + 4 * asymm
     )
     return coef
 
@@ -64,13 +91,23 @@ def betastable(A: int, colTerm: float, asymm: float, Mh=939.0, Mn=940.6) -> floa
 
     return temp
 
-def protondrip(A, Z, colterm, asymm, volTerm, surfaceTerm) -> float:
+
+def protondrip(A, colterm, asymm, volTerm, surfaceTerm) -> float:
 
     beta = coefbeta(A, colterm, asymm)
     gamma = coefgamma(A, volTerm, surfaceTerm, asymm, colterm)
 
-    proton = A * ((1 + beta) + np.sqrt((1 + beta) ** 2 + gamma))
+    proton = A * ((1 + beta) - np.sqrt((1 + beta) ** 2 - gamma))
     return proton
+
+
+def neutrondrip(A, volTerm, surfaceTerm, asymm, colTerm):
+
+    alpha = coefalpha(A, volTerm, surfaceTerm, colTerm, asymm)
+
+    neutron = A * (1 - np.sqrt(1 - alpha))
+    return neutron
+
 
 if __name__ == "__main__":
     main()
